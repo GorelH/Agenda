b8d8dcead15b48e25a46d9d2476904082fb37e78
kuwabara <kuwabara63@gmail.Com>
2013-04-02 07:43:52 +0200

    Mid

---
+++ WebSmartAgenda/Acceuil.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/index.Master" AutoEventWireup="true" CodeBehind="Acceuil.aspx.cs" Inherits="WebSmartAgenda.Acceuil" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Literal ID="introduction" runat="server"></asp:Literal>
+</asp:Content>
---
+++ WebSmartAgenda/Acceuil.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebSmartAgenda
+{
+    public partial class Acceuil : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            introduction.Text = "Bienvenue sur LE web agenda, pour suivre les infos du moment, réserver des places de concert, s'informer sur les artistes...";
+        }
+    }
+}
